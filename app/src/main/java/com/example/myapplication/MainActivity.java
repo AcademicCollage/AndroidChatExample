936462e2380e5ee5011ab5cdb72b571de14be661
Jeff <jeff@elector.co.il>
2021-05-05 19:41:42 +0300

    ...

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,49 +15,65 @@ import android.widget.MultiAutoCompleteTextView;
 import com.example.myapplication.objects.User;
 import com.example.myapplication.screens.Chat;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private EditText phone,name,password;
+    private EditText phone, name, password;
     private AutoCompleteTextView gender;
     private SharedPreferences sharedPreferences;
     private Button login;
-    private void init() {
-        initAutoComplete();
-        sharedPreferences=getSharedPreferences("userData",MODE_PRIVATE);
-        if(sharedPreferences.getString("phone","").length()>0){
-            goToNextActivity();
-        }
-        phone=findViewById(R.id.phone_number);
-        name=findViewById(R.id.name);
-        password=findViewById(R.id.password);
-        login =findViewById(R.id.login_button);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                db.
-                        collection("users").
-                        document(phone.getText().toString()).
-                        set(new User(name.getText().toString(),
-                                password.getText().toString(),
-                                phone.getText().toString(),
-                                gender.getText().toString())).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        sharedPreferences.edit().putString("phone",phone.getText().toString()).apply();
-                        goToNextActivity();
 
-                    }
-                });
-            }
-        });
+    private void init() {
+
+
+        sharedPreferences = getSharedPreferences("userData", MODE_PRIVATE);
+
+        if (sharedPreferences.getString("phone", "").length() > 0) {
+            db.collection("user").document(sharedPreferences.getString("phone", "")).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    Utils.user = documentSnapshot.toObject(User.class);
+                    goToNextActivity();
+                }
+            });
+        } else {
+            setContentView(R.layout.activity_main);
+            initAutoComplete();
+            phone = findViewById(R.id.phone_number);
+            name = findViewById(R.id.name);
+            password = findViewById(R.id.password);
+            login = findViewById(R.id.login_button);
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    User user = new User(name.getText().toString(),
+                            password.getText().toString(),
+                            phone.getText().toString(),
+                            gender.getText().toString());
+                    setContentView(R.layout.splash_screen);
+                    db.
+                            collection("users").
+                            document(phone.getText().toString()).
+                            set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            sharedPreferences.edit().putString("phone", phone.getText().toString()).apply();
+                            Utils.user = user;
+                            goToNextActivity();
+
+                        }
+                    });
+                }
+            });
+        }
 
 
     }
 
     private void goToNextActivity() {
-        Intent intent=new Intent(MainActivity.this, Chat.class);
+        Intent intent = new Intent(MainActivity.this, Chat.class);
         startActivity(intent);
         finish();
     }
@@ -65,19 +81,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
         init();
     }
-
 
 
     private void initAutoComplete() {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, GENDERS);
-         gender = findViewById(R.id.gender);
+        gender = findViewById(R.id.gender);
         gender.setAdapter(adapter);
     }
-    private static final String[] GENDERS = new String[] {
+
+    private static final String[] GENDERS = new String[]{
             "Male", "Female", "Unknown",
     };
 }
--- app/src/main/java/com/example/myapplication/screens/Chat.java
+++ app/src/main/java/com/example/myapplication/screens/Chat.java
@@ -5,33 +5,68 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.myapplication.R;
 import com.example.myapplication.RecyclerChat;
+import com.example.myapplication.Utils;
 import com.example.myapplication.objects.ChatConversation;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class Chat extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private RecyclerView recyclerView;
     private LottieAnimationView lottieAnimationView;
     private ArrayList<ChatConversation> chats;
+    private SharedPreferences sharedPreferences;
+    private EditText textOfUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         lottieAnimationView=findViewById(R.id.animationView);
         getChatsFromServer();
+        sendMessage();
+        sharedPreferences=getSharedPreferences("userData",MODE_PRIVATE);
 
+
+    }
+
+    private void sendMessage() {
+        ImageButton imageButton=findViewById(R.id.send_message);
+        EditText editText=findViewById(R.id.message_to_send);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+
+
+            @Override
+            public void onClick(View v) {
+                db.
+                        collection("chats")
+                        .document(""+new Date().getTime()).
+                        set(new ChatConversation(Utils.user.getName(),new Date().toGMTString(),editText.getText().toString(),""));
+
+            }
+        });
     }
 
     private void getChatsFromServer() {
@@ -43,6 +78,8 @@ public class Chat extends AppCompatActivity {
                     chats= (ArrayList<ChatConversation>) value.toObjects(ChatConversation.class);
                     if(chats.size()>0){
                         lottieAnimationView.setVisibility(View.GONE);
+                    }else{
+                        lottieAnimationView.setVisibility(View.VISIBLE);
                     }
 
                     setRecyclerView();
